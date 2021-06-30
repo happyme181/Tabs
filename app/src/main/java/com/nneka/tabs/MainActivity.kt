1406006381fb9ef5d9f1cb81f5205bcd7a13089c
happyme181 <nneka.eriavbe@gmail.com>
2021-06-30 18:00:17 +0100

    initial commit

---
+++ app/src/main/java/com/nneka/tabs/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.nneka.tabs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nneka.tabs.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private  lateinit var whatsAppAdapter: WhatsAppAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        whatsAppAdapter = WhatsAppAdapter(supportFragmentManager)
+        binding.pager.adapter = whatsAppAdapter
+        binding.tabLayout.setupWithViewPager(binding.pager)
+
+    }
+}
---
+++ app/src/main/java/com/nneka/tabs/WhatsAppAdapter.kt
@@ -0,0 +1,34 @@
+package com.nneka.tabs
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.nneka.tabs.views.CallsFragment
+import com.nneka.tabs.views.ChatsFragment
+import com.nneka.tabs.views.StatusFragment
+
+class WhatsAppAdapter(fm: FragmentManager): FragmentStatePagerAdapter (fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when (position){
+            0 -> return "Chats"
+            1 -> return "Status"
+            2 -> return "Calls"
+            else -> return  "Chats"
+        }
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when (position){
+            0 -> return ChatsFragment ()
+            1 -> return StatusFragment ()
+            2 -> return CallsFragment ()
+            else -> return ChatsFragment ()
+        }
+
+    }
+}
